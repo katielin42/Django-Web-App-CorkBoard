d3596f2006e2060025e84a11e51fb3367e36b3f6
j-vent <justine.ventura14@gmail.com>
2019-11-09 13:12:50 -0700

    Placeholder upload form

--- digposters/posters/models.py
+++ digposters/posters/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Poster(models.Model):
+    eventName = models.CharField(max_length = 50)
+    description = models.TextFiled(max_length = 150)
+
+
