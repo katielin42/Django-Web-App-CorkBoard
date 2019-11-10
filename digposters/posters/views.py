ee4433eb254a61942772d51e59b62932022d6320
justine <justine.ventura14@gmail.com>
2019-11-10 11:06:32 -0700

    Working Demo

--- digposters/posters/views.py
+++ digposters/posters/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import models
+from posters.forms import FilteredCategoryForm
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, SignUp
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 #renders the HTML page for home by pulling from the templates folder
@@ -21,6 +24,14 @@ class HomePageView(ListView):
     template_name = 'posters/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+    def get_queryset(self):
+        query = self.request.GET.get("selected")
+        print(query)
+        if(query is not None):
+            return Post.objects.filter(category_tag = query)
+        else:
+            return Post.objects.all()
 
 #individual page for poster (DOESNT WORK ATM because we didnt handle index shifting in query yet).
 class PostDetailView(DetailView):
@@ -46,4 +57,24 @@ class SignUpView(CreateView):
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+def filter_page(request):
+    form = FilteredCategoryForm()
+
+    if (request.method == 'POST'):
+        form = FilteredCategoryForm(data=request.POST)
+        if form.is_valid():
+            posters = Post.objects.all()
+            selection = form.cleaned_data['selected']
+            print(selection)
+            #selection = "Activity"
+            filtered = Post.objects.filter(category_tag=selection)
+            context = {
+                'posters': posters,
+                'selection': selection,
+            }
+            return redirect("poster:posters-home", selection)
+
+    return render(request, "posters/filter.html", {'form': form})
 
